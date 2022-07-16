9d3e3d5e3ff16f6fbec947792857e1babd2fccf0
duygus <dyg.calk@gmail.com>
2022-07-16 15:49:10 +0300

    Template Engine

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
 
-const PORT = 8080;
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
 
+//MIDDLEWARES
+app.use(express.static('public'));
+
+//ROUTES
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.send(blog);
+  res.render('index');
 });
 
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+app.get('/post', (req, res) => {
+  res.render('post');
+});
+const PORT = 8080;
 app.listen(PORT, () => {
   console.log('Server is up');
 });
