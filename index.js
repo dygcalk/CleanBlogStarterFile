b42eca0bbca6ebc9249ce38574f2821437062e96
duygus <dyg.calk@gmail.com>
2022-07-20 17:38:32 +0300

    mongoose model and req.body

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+const Post = require('./models/Post');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost/cleanblog-test-db');
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await Post.find();
+  res.render('index', { posts });
 });
 
 app.get('/about', (req, res) => {
@@ -23,6 +31,12 @@ app.get('/add_post', (req, res) => {
 app.get('/post', (req, res) => {
   res.render('post');
 });
+
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
+});
+
 const PORT = 8080;
 app.listen(PORT, () => {
   console.log('Server is up');
